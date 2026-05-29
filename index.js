b63fdb6394a51bd200c24f414139071c82bad78c
danil <danillevko1212@gmail.com>
2026-05-29 14:57:34 -0600

    media

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const url = fetch('https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/');
+const url = fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=vL6iA0hJeGlr3bPSJRzPoFqDQiqSCxqV');
+const template = fetch('./template.hbs');
+const compileTemplate = Handlebars.compile(template);
+
+
+function renderEvent(){
+    const eventConatiner = document.querySelector('.event-container');
+    
+}
